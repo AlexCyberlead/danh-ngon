caa1c3cc10d35d1288d9b394c660dce32164a8c2
anhtran278 <sanatram278@gmail.com>
2025-03-10 10:55:50 +0700

    add bootstrap - 30 data

--- src/quotes.js
+++ src/quotes.js
@@ -119,4 +119,124 @@ export const quotes = [
     author: "Walt Disney",
     year: "2025"
   },
+  {
+    id: 16,
+    image: "/images/thanhcong16.jpg",
+    tag: "Sự nghiệp",
+    quote: "Thành công không phải là đích đến, mà là cả một hành trình.",
+    author: "Arthur Ashe",
+    year: "2025"
+  },
+  {
+    id: 17,
+    image: "/images/thanhcong17.jpg",
+    tag: "Phát triển bản thân",
+    quote: "Đừng so sánh bản thân với người khác. Hãy so sánh với chính mình của ngày hôm qua.",
+    author: "Jordan Peterson",
+    year: "2025"
+  },
+  {
+    id: 18,
+    image: "/images/thanhcong18.jpg",
+    tag: "Thất bại",
+    quote: "Thất bại không phải là ngược lại với thành công, nó là một phần của thành công.",
+    author: "Arianna Huffington",
+    year: "2025"
+  },
+  {
+    id: 19,
+    image: "/images/thanhcong19.jpg",
+    tag: "Động lực",
+    quote: "Đừng chờ đợi cơ hội. Hãy tự tạo ra nó.",
+    author: "Steve Jobs",
+    year: "2025"
+  },
+  {
+    id: 20,
+    image: "/images/thanhcong20.jpg",
+    tag: "Học tập",
+    quote: "Học từ ngày hôm qua, sống cho ngày hôm nay, hy vọng cho ngày mai.",
+    author: "Albert Einstein",
+    year: "2025"
+  },
+  {
+    id: 21,
+    image: "/images/thanhcong21.jpg",
+    tag: "Khởi nghiệp",
+    quote: "Ý tưởng tốt nhất thường đến từ việc giải quyết vấn đề của chính bạn.",
+    author: "Mark Zuckerberg",
+    year: "2025"
+  },
+  {
+    id: 22,
+    image: "/images/thanhcong22.jpg",
+    tag: "Lãnh đạo",
+    quote: "Lãnh đạo không phải là về việc trở thành người giỏi nhất. Mà là về việc giúp người khác trở nên tốt hơn.",
+    author: "Simon Sinek",
+    year: "2025"
+  },
+  {
+    id: 23,
+    image: "/images/thanhcong23.jpg",
+    tag: "Sáng tạo",
+    quote: "Sáng tạo là trí thông minh đang vui chơi.",
+    author: "Albert Einstein",
+    year: "2025"
+  },
+  {
+    id: 24,
+    image: "/images/thanhcong24.jpg",
+    tag: "Kiên trì",
+    quote: "Người kiên trì sẽ chiến thắng người tài năng khi người tài năng từ bỏ.",
+    author: "Winston Churchill",
+    year: "2025"
+  },
+  {
+    id: 25,
+    image: "/images/thanhcong25.jpg",
+    tag: "Mục tiêu",
+    quote: "Mục tiêu không phải là để đạt được. Mà là để theo đuổi.",
+    author: "Bruce Lee",
+    year: "2025"
+  },
+  {
+    id: 26,
+    image: "/images/thanhcong26.jpg",
+    tag: "Thời gian",
+    quote: "Thời gian là thứ quý giá nhất bạn có thể tiêu. Hãy chắc chắn rằng bạn đang đầu tư nó một cách khôn ngoan.",
+    author: "Warren Buffett",
+    year: "2025"
+  },
+  {
+    id: 27,
+    image: "/images/thanhcong27.jpg",
+    tag: "Hạnh phúc",
+    quote: "Hạnh phúc không phải là điểm đến, mà là cách bạn đi.",
+    author: "Wayne Dyer",
+    year: "2025"
+  },
+  {
+    id: 28,
+    image: "/images/thanhcong28.jpg",
+    tag: "Thay đổi",
+    quote: "Thay đổi là quy luật của cuộc sống. Những người chỉ nhìn về quá khứ hay hiện tại chắc chắn sẽ bỏ lỡ tương lai.",
+    author: "John F. Kennedy",
+    year: "2025"
+  },
+  {
+    id: 29,
+    image: "/images/thanhcong29.jpg",
+    tag: "Ước mơ",
+    quote: "Đừng để ước mơ chỉ là ước mơ. Hãy đặt ra thời hạn và biến nó thành kế hoạch.",
+    author: "Walt Disney",
+    year: "2025"
+  },
+  {
+    id: 30,
+    image: "/images/thanhcong30.jpg",
+    tag: "Sự nghiệp",
+    quote: "Thành công không phải là đích đến, mà là cả một hành trình.",
+    author: "Arthur Ashe",
+    year: "2025"
+  },
 ];
